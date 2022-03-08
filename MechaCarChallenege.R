f46797887d34b8f70966ff7e098fb02221a254d0
Tony Wang <tonywang3571@gmail.com>
2022-03-08 11:53:33 -0600

    added suspension coil summary and lot summary

--- MechaCarChallenege.R
+++ MechaCarChallenege.R
@@ -1,11 +1,23 @@
 # Import dplyr library
 library(dplyr)
 
-# read csv file
-mecha_car <- read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors=F)
+# read mechacar_mpg csv file
+mecha_car <- read.csv(file="MechaCar_mpg.csv",check.names = F,stringsAsFactors = F)
 
 # multiple linear regression model
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car)
 
 # Summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
+
+# read suspension_coil csv file
+suspension_coil <- read.csv(file="Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+# total summary
+total_summary <- summarize(suspension_coil, mean(PSI), median(PSI), var(PSI), sd(PSI))
+total_summary
+
+# summary by lots
+lot_groups <- group_by(suspension_coil, Manufacturing_Lot)
+lot_summary <- summarize(lot_groups, mean(PSI), median(PSI), var(PSI), sd(PSI))
+lot_summary
