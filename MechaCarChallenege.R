2912cca98b7c77402365c116e28a720b711428cf
Tony Wang <tonywang3571@gmail.com>
2022-03-08 10:50:24 -0600

    added summary statistics to R file

--- MechaCarChallenege.R
+++ MechaCarChallenege.R
@@ -1,1 +1,11 @@
+# Import dplyr library
 library(dplyr)
+
+# read csv file
+mecha_car <- read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors=F)
+
+# multiple linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car)
+
+# Summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
